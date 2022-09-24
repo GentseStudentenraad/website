da779161eb463abfe85baa277eb42b8ff1819be9
Jens Pots <jens@jenspots.com>
2022-09-24 18:49:36 +0200

    Opinion class.

---
+++ src/lib/Opinion.ts
@@ -0,0 +1,31 @@
+import "chance";
+import { Chance } from "chance";
+
+export class OpinionGroup {
+    id!: number
+    rank!: number
+    name: string
+    about: string
+
+    static getAll() : Array<OpinionGroup> {
+        return []
+    }
+}
+
+export class Opinion {
+    id!: number
+    published_at!: Date
+    title!: string
+    about!: string
+    document_link!: string
+
+    static getOne() : Opinion {
+        return {
+            id: 0,
+            published_at: new Date(),
+            title: Chance().sentence(),
+            about: Chance().paragraph(),
+            document_link: "/404.pdf"
+        }
+    }
+}
