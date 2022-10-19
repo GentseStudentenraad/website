83ceee7964c56300cdf0b039ad04cdad668a1c6a
Jens Pots <jens@jenspots.com>
2022-10-19 13:56:11 +0200

    Remove unused route.

--- src/routes/[...language=language]/technical/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { getStores, navigating, page, updated } from '$app/stores';
-</script>
-
-<div class="container">
-	<p>{@html JSON.stringify($page, null, 4)}</p>
-</div>
