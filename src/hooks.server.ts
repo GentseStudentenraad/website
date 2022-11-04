abd9ad91f40f9a4ede694894ed8d797202ba9266
Jens Pots <jens@jenspots.com>
2022-11-04 14:22:57 +0100

    Moved hostnames into database.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,23 +5,6 @@ import type { MaybePromise } from '@sveltejs/kit/types/private';
 import { prisma } from '$lib/Prisma';
 import { Language } from '$lib/Language';
 
-const hosts = {
-	'gentsestudentenraad.be': Organization.GSR,
-	'ppsr.gentsestudentenraad.be': Organization.PPSR,
-	'fris.gentsestudentenraad.be': Organization.FRIS,
-	'stuw.gentsestudentenraad.be': Organization.STUW,
-	'sturec.gentsestudentenraad.be': Organization.STUREC,
-	'stuart.gentsestudentenraad.be': Organization.STUART,
-	'bsr.gentsestudentenraad.be': Organization.BSR,
-	'stuveco.gentsestudentenraad.be': Organization.STUVECO,
-	'stura.gentsestudentenraad.be': Organization.STURA,
-	'stubio.gentsestudentenraad.be': Organization.STUBIO,
-	'dsr.gentsestudentenraad.be': Organization.DSR,
-	'stugg.gentsestudentenraad.be': Organization.STUGG,
-	'stuff.gentsestudentenraad.be': Organization.STUFF,
-	localhost: Organization.GSR
-};
-
 // TODO: CAS authentication.
 // TODO: Should not default to GSR, but fine for development.
 export async function handle({
@@ -36,16 +19,10 @@ export async function handle({
 	// 2. The hostname contained in the request headers.
 	const requestedHost = event.url.searchParams.get('host') || event.url.hostname;
 
-	let organization: Organization | null = null;
+	const configs = await prisma.configuration.findMany();
+	let organization = configs.filter(c => c.hostnames.includes(requestedHost)).at(0)?.organization;
 
-	for (const [potentialHost, potentialOrganization] of Object.entries(hosts)) {
-		if (requestedHost == potentialHost) {
-			organization = potentialOrganization;
-			break;
-		}
-	}
-
-	if (organization === null) {
+	if (organization === undefined) {
 		return new Response(
 			'400: bad request. You have connected to the server using an invalid hostname.',
 			{ status: 400 }
