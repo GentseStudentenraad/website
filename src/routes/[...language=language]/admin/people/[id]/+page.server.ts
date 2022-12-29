3c749a9976b03d8a9f136a3b59cbd5d4c7fcad94
Jens Pots <jens@jenspots.com>
2022-12-29 17:27:37 +0100

    Basis people admin panel.

---
+++ src/routes/[...language=language]/admin/people/[id]/+page.server.ts
@@ -0,0 +1,26 @@
+import {prisma} from '$lib/Prisma';
+import type {PageServerLoad} from './$types';
+export const prerender = false;
+export const ssr = false;
+export const csr = true;
+
+export const load = (async ({ params, locals, url }) => {
+	const _ = params.language; // SVELTEKIT BUG, DO NOT REMOVE
+
+	const person = await prisma.person.findUnique({
+		include: {
+			positions: {
+				include: {
+					person_group: true,
+				}
+			}
+		},
+		where: {
+			id: parseInt(params.id)
+		}
+	});
+
+	return {
+		person
+	};
+}) satisfies PageServerLoad;
