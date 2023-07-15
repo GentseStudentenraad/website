6f243caff2852bb4ad886b06be8e52b3f3491b82
Jens Pots <jens@jenspots.com>
2023-07-15 12:37:08 +0200

    feat: create and delete i18n

--- src/routes/[...language=language]/admin/i18n/+page.svelte
+++ src/routes/[...language=language]/admin/i18n/+page.svelte
@@ -1,51 +1,71 @@
 <script lang="ts">
     import type { PageData } from "./$types";
+    import NewButton from "$lib/components/admin/NewButton.svelte";
+    import { i18n } from "@prisma/client";
 
     export let data: PageData;
+
+    async function remove(element: i18n) {
+        const res = await fetch(`/api/i18n/${element.id}`, {
+            method: "DELETE",
+        });
+
+        if (res.status === 200) {
+            location.reload();
+        } else {
+            alert(JSON.stringify(res, null, 2));
+        }
+    }
 </script>
 
-<table class="table-auto text-left w-full">
-    <thead>
-        <tr>
-            <th>
-                <div class="flex items-center">
-                    <p>Tag</p>
-                    <div class="grow" />
-                    <i class="bi bi-chevron-down" />
-                </div>
-            </th>
-            <th>
-                <div class="flex items-center">
-                    <p>Type</p>
-                    <div class="grow" />
-                    <i class="bi bi-chevron-down" />
-                </div>
-            </th>
-            <th>Nederlands</th>
-            <th>Engels</th>
-            <th />
-            <th />
-        </tr>
-    </thead>
-    <tbody>
-        {#each data.texts as text}
+<div class="space-y-4">
+    <table class="table-auto text-left w-full">
+        <thead>
             <tr>
-                <td>{text.key}</td>
-                <td>{text.markup}</td>
-                <td>{text.dutch?.substring(0, 50)}</td>
-                <td>{text.english?.substring(0, 50)}</td>
-                <td>
-                    <a href="/admin/i18n/{text.id}">
-                        <i class="bi bi-pencil-square" />
-                    </a>
-                </td>
-                <td>
-                    <i class="bi bi-trash3-fill" />
-                </td>
+                <th>
+                    <div class="flex items-center">
+                        <p>Tag</p>
+                        <div class="grow" />
+                        <i class="bi bi-chevron-down" />
+                    </div>
+                </th>
+                <th>
+                    <div class="flex items-center">
+                        <p>Type</p>
+                        <div class="grow" />
+                        <i class="bi bi-chevron-down" />
+                    </div>
+                </th>
+                <th>Nederlands</th>
+                <th>Engels</th>
+                <th />
+                <th />
             </tr>
-        {/each}
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            {#each data.texts as text}
+                <tr>
+                    <td>{text.key}</td>
+                    <td>{text.markup}</td>
+                    <td>{text.dutch?.substring(0, 50)}</td>
+                    <td>{text.english?.substring(0, 50)}</td>
+                    <td>
+                        <a href="/admin/i18n/{text.id}">
+                            <i class="bi bi-pencil-square" />
+                        </a>
+                    </td>
+                    <td>
+                        <button on:click={() => remove(text)}>
+                            <i class="bi bi-trash3-fill" />
+                        </button>
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+
+    <NewButton href="/admin/i18n/create" />
+</div>
 
 <style lang="sass">
     th
---
+++ src/routes/[...language=language]/admin/i18n/create/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import TextField from "$lib/components/admin/TextField.svelte";
+    import LongTextField from "$lib/components/admin/LongTextField.svelte";
+    import ActionButton from "$lib/components/admin/ActionButton.svelte";
+    import type { PageData } from "./$types";
+    import { goto } from "$app/navigation";
+    import { Prisma } from "@prisma/client";
+
+    export let data: PageData;
+
+    let newI18n: Prisma.i18nCreateInput = {
+        key: "",
+        dutch: "",
+        english: "",
+        markup: "PLAIN_TEXT",
+        organization: data.configuration.organization,
+    };
+
+    async function post() {
+        const res = await fetch(`/api/i18n/`, {
+            method: "POST",
+            body: JSON.stringify(newI18n),
+            headers: {
+                "content-type": "application/json",
+            },
+        });
+
+        if (res.status === 200) {
+            await goto("/admin/i18n");
+        } else {
+            alert(JSON.stringify(res, null, 2));
+        }
+    }
+</script>
+
+<div class="container space-y-4">
+    <div>
+        <p class="text-[12px] opacity-50 font-semibold uppercase">Type</p>
+        <select>
+            <option>MARKDOWN</option>
+            <option>PLAIN_TEXT</option>
+        </select>
+    </div>
+    <TextField description="Identificatie" bind:value={newI18n.key} />
+    <LongTextField description="Nederlands" bind:value={newI18n.dutch} />
+    <LongTextField description="Engels" bind:value={newI18n.english} />
+    <ActionButton action={post} />
+</div>
